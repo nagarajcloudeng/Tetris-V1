13a2fd95eb4b579dbb54aebc11a6e396ac0ad669
nagaraj <iamnagaraj13@gmail.com>
2024-12-20 12:37:12 +0530

    changes in jenkins file

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "eks-jenkins-24" # Replace with your actual S3 bucket name
     key    = "Jen-eks/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
